c8c5e6b6689aaf1b6826e21775627b6d7d2de2e9
noramari <noora@selinan.com>
2023-06-01 17:00:45 +0900

    Initial Commit

---
+++ SwiftUIUltimatePortfolio/SwiftUIUltimatePortfolioApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIUltimatePortfolioApp.swift
+//  SwiftUIUltimatePortfolio
+//
+//  Created by Noora Maeda on 2023/06/01.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIUltimatePortfolioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
