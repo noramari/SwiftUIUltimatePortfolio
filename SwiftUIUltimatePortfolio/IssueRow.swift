f3db2dfcabc68cca09ae84c65d53da21fdfb85c1
noramari <noora@selinan.com>
2023-06-02 23:11:12 +0900

    Edit Core Data Merge, add model helpers and add UI for issues

---
+++ SwiftUIUltimatePortfolio/IssueRow.swift
@@ -0,0 +1,53 @@
+//
+//  IssueRow.swift
+//  SwiftUIUltimatePortfolio
+//
+//  Created by Noora Maeda on 2023/06/02.
+//
+
+import SwiftUI
+
+struct IssueRow: View {
+    @EnvironmentObject var dataController: DataController
+    @ObservedObject var issue: Issue
+    
+    var body: some View {
+        NavigationLink(value: issue) {
+            HStack {
+                Image(systemName: "exclamationmark.circle")
+                    .imageScale(.large)
+                    .opacity(issue.priority == 2 ? 1 : 0)
+                
+                VStack(alignment: .leading) {
+                    Text(issue.issueTitle)
+                        .font(.headline)
+                        .lineLimit(1)
+                    
+                    Text("No tags")
+                        .foregroundStyle(.secondary)
+                        .lineLimit(1)
+                }
+                
+                Spacer()
+                
+                VStack(alignment: .trailing) {
+                    Text(issue.issueCreationDate.formatted(date: .abbreviated, time: .omitted))
+                        .font(.subheadline)
+                    
+                    if issue.completed {
+                        Text("CLOSED")
+                            .font(.body.smallCaps())
+                    }
+                }
+                .foregroundStyle(.secondary)
+            }
+        }
+        
+    }
+}
+
+struct IssueRow_Previews: PreviewProvider {
+    static var previews: some View {
+        IssueRow(issue: .example)
+    }
+}
---
+++ SwiftUIUltimatePortfolio/IssueView.swift
@@ -0,0 +1,20 @@
+//
+//  IssueView.swift
+//  SwiftUIUltimatePortfolio
+//
+//  Created by Noora Maeda on 2023/06/02.
+//
+
+import SwiftUI
+
+struct IssueView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct IssueView_Previews: PreviewProvider {
+    static var previews: some View {
+        IssueView()
+    }
+}
